5bb39846fe2492b042433a6d3e53ad38f5fbba1c
ThatGuyJamal <43446907+ThatGuyJamal@users.noreply.github.com>
2022-03-08 00:36:11 -0500

    Update updateCommands.ts

--- packages/helpers/updateCommands.ts
+++ packages/helpers/updateCommands.ts
@@ -1,43 +1,14 @@
-import { configs } from "../../configs.ts";
 import { Bot } from "../../bot.ts";
 import { log } from "../utils/log/logger.ts";
 import { MakeRequired } from "https://deno.land/x/discordeno@13.0.0-rc20/types/util.ts";
 import { EditGlobalApplicationCommand, upsertApplicationCommands } from "@deps";
-
-/**
- * Updates slash commands on the discord api
- */
-export async function updateApplicationCommands() {
-  log.info("Updating application commands");
-  await globalCommand();
-  await guildCommand();
-}
-
-async function globalCommand() {
-  /** Checks if command is global or not */
-  await Bot.helpers.upsertApplicationCommands(
-    Bot.commands
-      /** Filters out commands that are not devOnly */
-      .filter((command) => !(command.devOnly ?? false))
-      .array(),
-  );
-}
-
-async function guildCommand() {
-  /** Checks if command is guild only or not */
-  await Bot.helpers.upsertApplicationCommands(
-    Bot.commands
-      .filter((command) => !!command.devOnly)
-      .array(),
-    configs.devGuildId,
-  );
-}
+import { DiscordenoGuild } from "https://deno.land/x/discordeno@13.0.0-rc20/transformers/guild.ts";
 
 /**
  * Updates the command information in the discord api
  * @param scope The scope of the command | options are "global" or "guild"
  */
-export async function uploadCommands(scope: "Guild" | "Global") {
+export async function updateApplicationCommands(scope?: "Guild" | "Global") {
   const globalCommands: MakeRequired<EditGlobalApplicationCommand, "name">[] = [];
   const perGuildCommands: MakeRequired<EditGlobalApplicationCommand, "name">[] = [];
 
@@ -80,6 +51,28 @@ export async function uploadCommands(scope: "Guild" | "Global") {
     log.info(
       "Updating Guild Commands, this takes up to 1 minute to take effect...",
     );
-     Bot.guilds.forEach((guild) => upsertApplicationCommands(Bot, perGuildCommands, guild.id))
+    Bot.guilds.forEach((guild) => upsertApplicationCommands(Bot, perGuildCommands, guild.id));
+  }
+}
+
+/** Update commands for a guild */
+export async function updateGuildCommands(guild: DiscordenoGuild) {
+  const perGuildCommands: MakeRequired<EditGlobalApplicationCommand, "name">[] = [];
+
+  for (const command of Bot.commands.values()) {
+    if (command.scope) {
+      if (command.scope === "Guild") {
+        perGuildCommands.push({
+          name: command.name,
+          description: command.description,
+          type: command.type,
+          options: command.options ? command.options : undefined,
+        });
+      }
+    }
+  }
+
+  if (perGuildCommands.length) {
+    await upsertApplicationCommands(Bot, perGuildCommands, guild.id);
   }
 }
