39e3a8325cd1f37f095459fb5a7ee44f244da6a2
ThatGuyJamal <43446907+ThatGuyJamal@users.noreply.github.com>
2022-03-08 11:08:38 -0500

    fmt

--- mod.ts
+++ mod.ts
@@ -2,18 +2,17 @@ import { startBot } from "./deps.ts";
 import { log } from "@utils";
 import { updateApplicationCommands } from "./packages/helpers/updateCommands.ts";
 import { Bot } from "@bot";
-import {fileLoader, importDirectory} from "./packages/helpers/loader.ts";
+import { fileLoader, importDirectory } from "./packages/helpers/loader.ts";
 
 log.info("Starting bot...");
 
 /** Handles our modules to load with deno */
 // Forces deno to read all the files which will fill the commands/inhibitors cache etc.
 await Promise.all(
-    [
-        "./bot/commands",
-        "./bot/events",
-        // "./src/tasks",
-    ].map((path: string) => importDirectory(Deno.realPathSync(path))),
+  [
+    "./bot/commands",
+    "./bot/events",
+  ].map((path: string) => importDirectory(Deno.realPathSync(path))),
 );
 await fileLoader();
 
