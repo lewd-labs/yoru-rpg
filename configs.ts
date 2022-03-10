41918518c00044b3a51f123e26d6a2c6cebedd79
ThatGuyJamal <43446907+ThatGuyJamal@users.noreply.github.com>
2022-03-09 23:44:26 -0500

    Update configs.ts

--- configs.ts
+++ configs.ts
@@ -19,10 +19,20 @@ export const configs = {
   botId: BigInt(atob(token.split(".")[0])),
   /** The server id where you develop your bot and want dev commands created. */
   devGuildId: BigInt(env.DEV_GUILD_ID!),
+  supportServerId: "837830514130812970",
   invite_url:
     "https://discord.com/api/oauth2/authorize?client_id=946398697254703174&permissions=466812794944&scope=bot%20applications.commands",
   applicationId: BigInt(946398697254703174n),
   development_mode: true,
+  userIds: {
+    // You can delete the `as string[]` when you add least 1 id in them.
+    // The user ids for the support team
+    botSupporters: [] as string[],
+    // The user ids for the other devs on your team
+    botDevs: [] as string[],
+    // The user ids who have complete 100% access to your bot
+    botOwners: [] as string[],
+  },
 };
 
 export const MISSING_TRANSLATION_WEBHOOK = env.MISSING_TRANSLATION_WEBHOOK ||
